cf4a0d163f92b6761fcbb3105633d9e4af4a9c75
Rob <rsaund05@uoguelph.ca>
2018-11-20 12:02:49 -0500

    Revert "??"
    This reverts commit 1148ff6e3b2da65e310ea89b26c76f6fd05e56bb.

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -24,7 +24,6 @@ public class Meal{
             return nil
         }
         
-        //Rating must be a valid value
         guard (rating >= 0)  && (rating <= 5) else {
             return nil
         }
